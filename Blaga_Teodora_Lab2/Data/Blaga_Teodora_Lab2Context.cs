d9e21e3ec10d9921cd61bc042f1f1fd6edb3e021
YONDER\tblaga <teodora.blaga@tss-yonder.com>
2024-10-22 19:14:00 +0300

    Lab2

---
+++ Blaga_Teodora_Lab2/Data/Blaga_Teodora_Lab2Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Blaga_Teodora_Lab2.Models;
+
+namespace Blaga_Teodora_Lab2.Data
+{
+    public class Blaga_Teodora_Lab2Context : DbContext
+    {
+        public Blaga_Teodora_Lab2Context (DbContextOptions<Blaga_Teodora_Lab2Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Blaga_Teodora_Lab2.Models.Book> Book { get; set; } = default!;
+    }
+}
--- Blaga_Teodora_Lab2/Program.cs
+++ Blaga_Teodora_Lab2/Program.cs
@@ -1,7 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Blaga_Teodora_Lab2.Data;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+builder.Services.AddDbContext<Blaga_Teodora_Lab2Context>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("Blaga_Teodora_Lab2Context") ?? throw new InvalidOperationException("Connection string 'Blaga_Teodora_Lab2Context' not found.")));
 
 var app = builder.Build();
 
