7ddfd921ac15e4a7b513e90f266a33d8f8be27db
YONDER\tblaga <teodora.blaga@tss-yonder.com>
2024-12-10 19:33:35 +0200

    Lab4 finished

---
+++ Blaga_Teodora_Lab2/Models/ViewModels/CategoryIndexData.cs
@@ -0,0 +1,9 @@
+﻿namespace Blaga_Teodora_Lab2.Models.ViewModels
+{
+    public class CategoryIndexData
+    {
+        public IEnumerable<Category> Categories { get; set; }
+        public IEnumerable<BookCategory> BookCategories { get; set; }
+        public IEnumerable<Book> Books { get; set; }
+    }
+}
--- Blaga_Teodora_Lab2/Pages/Categories/Index.cshtml.cs
+++ Blaga_Teodora_Lab2/Pages/Categories/Index.cshtml.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using Blaga_Teodora_Lab2.Data;
 using Blaga_Teodora_Lab2.Models;
+using Blaga_Teodora_Lab2.Models.ViewModels;
 
 namespace Blaga_Teodora_Lab2.Pages.Categories
 {
@@ -21,9 +22,40 @@ namespace Blaga_Teodora_Lab2.Pages.Categories
 
         public IList<Category> Category { get;set; } = default!;
 
-        public async Task OnGetAsync()
+        public CategoryIndexData CategoryData { get; set; }
+        public int CategoryID { get; set; }
+        public int BookID { get; set; }
+        public int BookCategoryID { get; set; }
+
+        public IEnumerable<Book> books;
+
+        public async Task OnGetAsync(int? id, int? bookID)
         {
-            Category = await _context.Category.ToListAsync();
+            books = new List<Book>();
+            CategoryData = new CategoryIndexData();
+
+            CategoryData.Categories = await _context.Category
+                .Include(i => i.BookCategories)
+                .ThenInclude(i => i.Book)
+                .ThenInclude(i => i.Author)
+                .OrderBy(i => i.CategoryName)
+                .ToListAsync();
+
+            if (id != null)
+            {
+                CategoryID = id.Value;
+                Category category = CategoryData.Categories
+                .Where(i => i.ID == id.Value).Single();
+                CategoryData.BookCategories = category.BookCategories;
+
+                foreach (BookCategory bookCategory in CategoryData.BookCategories)
+                {
+                    Book book = bookCategory.Book;
+                    books.Append(book);
+                }
+
+                CategoryData.Books = books;
+            }
         }
     }
 }
