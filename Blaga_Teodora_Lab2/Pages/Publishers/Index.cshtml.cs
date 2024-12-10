21f3f82adc61da35de27002cf52f8c26e0a30ccb
YONDER\tblaga <teodora.blaga@tss-yonder.com>
2024-12-10 18:10:15 +0200

    Lab4

--- Blaga_Teodora_Lab2/Pages/Publishers/Index.cshtml.cs
+++ Blaga_Teodora_Lab2/Pages/Publishers/Index.cshtml.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using Blaga_Teodora_Lab2.Data;
 using Blaga_Teodora_Lab2.Models;
+using Blaga_Teodora_Lab2.Models.ViewModels;
 
 namespace Blaga_Teodora_Lab2.Pages.Publishers
 {
@@ -18,12 +19,28 @@ namespace Blaga_Teodora_Lab2.Pages.Publishers
         {
             _context = context;
         }
+        public IList<Publisher> Publisher { get; set; } = default!;
+        public PublisherIndexData PublisherData { get; set; }
+        public int PublisherID { get; set; }
+        public int BookID { get; set; }
 
-        public IList<Publisher> Publisher { get;set; } = default!;
-
-        public async Task OnGetAsync()
+        public async Task OnGetAsync(int? id, int? bookID)
         {
-            Publisher = await _context.Publisher.ToListAsync();
+            PublisherData = new PublisherIndexData();
+
+            PublisherData.Publishers = await _context.Publisher
+                .Include(i => i.Books)
+                .ThenInclude(c => c.Author)
+                .OrderBy(i => i.PublisherName)
+                .ToListAsync();
+
+            if (id != null)
+            {
+                PublisherID = id.Value;
+                Publisher publisher = PublisherData.Publishers
+                .Where(i => i.ID == id.Value).Single();
+                PublisherData.Books = publisher.Books;
+            }
         }
     }
 }
